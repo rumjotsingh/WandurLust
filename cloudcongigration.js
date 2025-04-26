f58a0f4e488a78f21c4869021981cf014ea1f02a
rumjotsingh <155811126+rumjotsingh@users.noreply.github.com>
2025-04-26 15:19:43 +0530

    Update cloudcongigration.js

--- cloudcongigration.js
+++ cloudcongigration.js
@@ -1,5 +1,7 @@
 const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
+const dotenv = require("dotenv");
+dotenv.config();
 cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
     api_key: process.env.CLOUD_API_KEY,
